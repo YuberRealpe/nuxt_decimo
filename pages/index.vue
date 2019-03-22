a6a6de302318b76252cc19fa60a65b9480fdc11b
Yuber Realpe <andresrealp97@gmail.com>
2019-03-21 20:26:34 -0500

    Iniciando directorio

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Hola {{ msg }}</h1>
+    <input type="text" v-model="msg">
+    <button @click="guardar">Guardar</button>
+    <table>
+        <tr>
+            <th>Nombre</th>
+        </tr>
+        <tr v-for="(item,index) in municipios" :key="index">
+            <td>{{item}}</td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Yuber Realpe",
+      municipios: ["mocoa", "villagarzon", "puerto asis"]
+    };
+  },
+  methods:{
+      guardar(){
+          this.municipios.push(this.msg)
+      }
+  }
+};
+</script>
+
+<style>
+</style>
